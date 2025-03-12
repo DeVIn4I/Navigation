a82fb94b63ac0b39b0d2016af65dd7f15504cace
Razumov Pavel <razumov_pv@mail.ru>
2025-03-12 13:46:15 +0300

    Finish hw-1

---
+++ Navigation/Screens/Feed/Controllers/FeedViewController.swift
@@ -0,0 +1,53 @@
+//
+//  FeedViewController.swift
+//  Navigation
+//
+//  Created by Razumov Pavel on 12.03.2025.
+//
+
+import UIKit
+
+final class FeedViewController: UIViewController {
+    
+    private let post: Post
+    private lazy var showPostButton: CustomButton = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.addTarget(self, action: #selector(showPostButtonTapped), for: .touchUpInside)
+        return $0
+    }(CustomButton(title: "Show post"))
+    
+    init(post: Post) {
+        self.post = post
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpViews()
+        setConstraints()
+    }
+    
+    private func setUpViews() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(showPostButton)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            showPostButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            showPostButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    @objc
+    private func showPostButtonTapped() {
+        let postVC = PostViewController(post: post)
+        postVC.modalPresentationStyle = .fullScreen
+        postVC.modalTransitionStyle = .coverVertical
+        navigationController?.pushViewController(postVC, animated: true)
+    }
+}
---
+++ Navigation/Screens/Feed/Models/Post.swift
@@ -0,0 +1,12 @@
+//
+//  Post.swift
+//  Navigation
+//
+//  Created by Razumov Pavel on 12.03.2025.
+//
+
+import Foundation
+
+struct Post {
+    let title: String
+}
