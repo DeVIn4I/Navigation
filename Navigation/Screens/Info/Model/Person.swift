40fa3e7e2e2afc7662fef1e5ddddeca779d7bd9d
Razumov Pavel <razumov_pv@mail.ru>
2025-07-09 10:37:39 +0300

    finish hw 1.2

---
+++ Navigation/Screens/Info/Model/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  Navigation
+//
+//  Created by Razumov Pavel on 09.07.2025.
+//
+
+import Foundation
+
+struct Person: Codable {
+    let id: Int
+    let name: String
+    let username: String
+    let email: String
+}
